cba5b73bd0812c3020295efc7baa3939b96d0951
Doug B <dougy.b@proton.me>
2024-01-13 09:00:02 +0000

    Deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tech_treasures.wsgi:application
--- tech_treasures/settings.py
+++ tech_treasures/settings.py
@@ -103,12 +103,17 @@ WSGI_APPLICATION = 'tech_treasures.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
