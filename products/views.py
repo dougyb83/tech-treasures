fd8ea9bfad931f38f4c6291f5647625b7e5958a5
Doug B <dougy.b@proton.me>
2024-01-17 07:14:59 +0000

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
+
     page = request.GET.get("page", 1)
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     paginator = Paginator(products, 12)
     try:
         products = paginator.page(page)
@@ -14,8 +29,10 @@ def all_products(request):
         products = paginator.page(1)
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
+
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
