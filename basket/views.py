4c85f7fd9c968ff20867e1beeaf516623b7441d7
Doug B <dougy.b@proton.me>
2024-01-20 07:27:08 +0000

    Show basket total and update basket items

--- basket/views.py
+++ basket/views.py
@@ -20,5 +20,4 @@ def add_to_basket(request, item_id):
         basket[item_id] = quantity
 
     request.session['basket'] = basket
-    print(request.session['basket'])
     return redirect(redirect_url)
