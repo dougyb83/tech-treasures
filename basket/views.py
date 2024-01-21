bac8734c383c3b09c928ea7c0867368808fc79a0
Doug B <dougy.b@proton.me>
2024-01-21 09:29:22 +0000

    Add toast messages

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_basket(request):
@@ -12,6 +15,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     options = None
@@ -23,15 +27,29 @@ def add_to_basket(request, item_id):
         if item_id in list(basket.keys()):
             if options in basket[item_id]['items_by_options'].keys():
                 basket[item_id]['items_by_options'][options] += quantity
+                messages.success(request,
+                                 (f'Updated {options.upper()} '
+                                  f'{product.name} quantity to '
+                                  f'{basket[item_id]["items_by_options"][options]}'))
             else:
                 basket[item_id]['items_by_options'][options] = quantity
+                messages.success(request,
+                                 (f'Added {options.upper()} '
+                                  f'{product.name} to your basket'))
         else:
             basket[item_id] = {'items_by_options': {options: quantity}}
+            messages.success(request,
+                             (f'Added {options.upper()} '
+                              f'{product.name} to your basket'))
     else:
         if item_id in list(basket.keys()):
             basket[item_id] += quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {basket[item_id]}'))
         else:
             basket[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -40,7 +58,7 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
-    # product = get_object_or_404(Product, pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     options = None
     if 'product_options' in request.POST:
@@ -50,15 +68,28 @@ def adjust_basket(request, item_id):
     if options:
         if quantity > 0:
             basket[item_id]['items_by_options'][options] = quantity
+            messages.success(request,
+                             (f'Updated {options.upper()} '
+                              f'{product.name} quantity to '
+                              f'{basket[item_id]["items_by_options"][options]}'))
         else:
             del basket[item_id]['items_by_options'][options]
             if not basket[item_id]['items_by_options']:
                 basket.pop(item_id)
+            messages.success(request,
+                             (f'Removed {options.upper()} '
+                              f'{product.name} from your basket'))
     else:
         if quantity > 0:
             basket[item_id] = quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {basket[item_id]}'))
         else:
             basket.pop(item_id)
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your basket'))
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -68,7 +99,7 @@ def remove_from_basket(request, item_id):
     """Remove the item from the shopping basket"""
 
     try:
-        # product = get_object_or_404(Product, pk=item_id)
+        product = get_object_or_404(Product, pk=item_id)
         options = None
         if 'product_options' in request.POST:
             options = request.POST['product_options']
@@ -78,11 +109,16 @@ def remove_from_basket(request, item_id):
             del basket[item_id]['items_by_options'][options]
             if not basket[item_id]['items_by_options']:
                 basket.pop(item_id)
+            messages.success(request,
+                             (f'Removed {options.upper()} '
+                              f'{product.name} from your basket'))
         else:
             basket.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
