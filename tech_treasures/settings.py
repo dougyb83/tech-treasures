42672d762ec14e0bd6a279e851b9dfe307156f32
Doug B <dougy.b@proton.me>
2024-01-24 06:31:49 +0000

    Add checkout template

--- tech_treasures/settings.py
+++ tech_treasures/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +70,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tech_treasures.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,8 +86,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
