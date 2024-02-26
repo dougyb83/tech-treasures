6bfb5e39f3f731c2cfecdf19b4fd6c076d8c3278
Doug B <dougy.b@proton.me>
2024-02-26 07:59:31 +0000

    Add error page content

--- tech_treasures/settings.py
+++ tech_treasures/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 host = os.environ.get("SITE")
