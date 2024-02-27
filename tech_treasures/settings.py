418531517ff80a90cc81736e5b9d0ae951b7b73a
Doug B <dougy.b@proton.me>
2024-02-27 07:46:54 +0000

    Add condition for DEBUG

--- tech_treasures/settings.py
+++ tech_treasures/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if 'DEBUG' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 host = os.environ.get("SITE")
